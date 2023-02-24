fe9039406f84aabb1bfd4fd7c49492277f52c118
Tino <tino5280@gmail.com>
2023-02-23 18:37:38 -0700

    commit tree 2ee6512d596581eda61836b565353a4c7906e879

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNum) {
+    if (blockNum > 42) {
+        return blockNum - 42;
+    }
+    else {
+        return 42 - blockNum;
+    }
+}
+
+function distanceFromHqInFeet(blockNum) {
+    return distanceFromHqInBlocks(blockNum) * 264
+}
+
+function distanceTravelledInFeet(blockNum1, blockNum2) {
+    if (blockNum1 > blockNum2) {
+        return (blockNum1 - blockNum2) * 264
+    }
+    else 
+    return (blockNum2 - blockNum1) * 264
+}
